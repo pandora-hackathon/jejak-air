f6830cbd0ce5d610961900940c24235a8b212080
applepiesss <nadiaaiiisyahf@gmail.com>
2025-11-09 00:08:25 +0700

    Completed logic for auth

---
+++ authentication/forms.py
@@ -0,0 +1,13 @@
+
+from authentication.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+class RegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class LoginForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
