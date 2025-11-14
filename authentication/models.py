a7593de7a3b221f00f12839fdd00f931aee95857
kurrorro <keishavnia@gmail.com>
2025-11-14 20:08:30 +0700

    modify models to auto set superuser role to admin

--- authentication/models.py
+++ authentication/models.py
@@ -1,7 +1,20 @@
 import uuid
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager as DjangoUserManager
 
+class CustomUserManager(DjangoUserManager):
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("role", "admin")
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return super().create_superuser(username, email, password, **extra_fields)
+    
 class User(AbstractUser):
     ROLE_CHOICES = [
         ('admin', 'Admin'),
@@ -14,6 +27,9 @@ class User(AbstractUser):
     role = models.CharField(max_length=15, choices=ROLE_CHOICES, default='farmOwner')
     has_profile = models.BooleanField(default=False)
 
+    objects = CustomUserManager()
+
     def __str__(self):
         return self.username
+
 
