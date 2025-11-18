68b8154990bddded42dc592d2a71221a81cfec5e
kurrorro <keishavnia@gmail.com>
2025-11-19 02:13:55 +0700

    landing page

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ from main.views import *
 app_name = 'main'
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', landing_page, name='landing'),
     path('dashboard/', views.dashboard_switcher, name='dashboard_switcher'), 
     path('dashboard/qc/', views.dashboard_qc, name='dashboard_qc'),
 ]
